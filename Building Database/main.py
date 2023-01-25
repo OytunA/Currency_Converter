73ac8fe873d84262a330a5ec695a3bdaf3452d0f
OytunA <oytun.avunduk@ozu.edu.tr>
2023-01-26 00:27:39 +0300

    Extract

--- Building Database/main.py
+++ Building Database/main.py
@@ -2,10 +2,10 @@ import csv
 import config
 from helpers.mongodb import get_mongo_client
 
-db = get_mongo_client(config.mongo_username, config.mongo_password, config.mongo_clustername, config.mongo_clusterlink,
-                      config.mongo_clientname)
+database = get_mongo_client(config.mongo_username, config.mongo_password, config.mongo_clustername,
+                            config.mongo_clusterlink, config.mongo_clientname)
 
-collection = db[config.mongo_collectionname]
+collection = database[config.mongo_collectionname]
 
 csv_filename = config.data_path
 with open(csv_filename, encoding="utf8") as f:
