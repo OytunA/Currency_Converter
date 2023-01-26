576cdda2f679aa0a4c3911e57e19e0ad75b2f205
OytunA <oytun.avunduk@ozu.edu.tr>
2023-01-27 01:18:56 +0300

    Helper to connect MongoDB

--- helpers/mongodb.py
+++ helpers/mongodb.py
@@ -5,3 +5,4 @@ def get_mongo_client(username, password, clustername, clusterlink, clientname):
     connection_string = f"mongodb+srv://{username}:{password}@{clustername}.{clusterlink}.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(connection_string)
     return client[clientname]
+
