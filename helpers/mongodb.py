c9a49390b467ad32fba67436bb46979748b570b4
OytunA <oytun.avunduk@ozu.edu.tr>
2023-01-31 03:37:35 +0300

    Helper to connect and get data with JSON from mongodb

--- helpers/mongodb.py
+++ helpers/mongodb.py
@@ -1,8 +1,24 @@
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 
 def get_mongo_client(username, password, clustername, clusterlink, clientname):
+    # Initializing connection for mongodb
     connection_string = f"mongodb+srv://{username}:{password}@{clustername}.{clusterlink}.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(connection_string)
     return client[clientname]
 
+
+def get_data_from_mongodb(username, password, clustername, clusterlink, clientname, collectionname):
+    # Accessing mongodb database
+    database = get_mongo_client(username, password, clustername, clusterlink, clientname)
+
+    # Deciding collection
+    default_collection = database[collectionname]
+
+    # Transforming data to JSON file
+    data = default_collection.find()
+    mongodb_docs = list(data)
+    json_data = dumps(mongodb_docs)
+
+    return json_data
